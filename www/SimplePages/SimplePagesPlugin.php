52af3001e0bd001cf7e0245bfc0efff24e99e5c4
Ben <merc96github@gmail.com>
2017-10-09 19:04:42 +0100

    Modded simple pages plugin added.

--- www/SimplePages/SimplePagesPlugin.php
+++ www/SimplePages/SimplePagesPlugin.php
@@ -16,7 +16,7 @@ class SimplePagesPlugin extends Omeka_Plugin_AbstractPlugin
     /**
      * @var array Hooks for the plugin.
      */
-    protected $_hooks = array('install', 'uninstall', 'upgrade', 'initialize',
+    protected $_hooks = array('install', 'uninstall',
         'define_acl', 'define_routes', 'config_form', 'config',
         'html_purifier_form_submission');
 
@@ -76,7 +76,39 @@ class SimplePagesPlugin extends Omeka_Plugin_AbstractPlugin
         $page->parent_id = 0;
         $page->title = 'About';
         $page->slug = 'about';
-        $page->text = '<p></p>';
+        $page->text = '<p>Communication about content, requests and additions to this catalogue can be addressed to
+“psych_datasets@lancaster.ac.uk"
+
+
+The LUSTRE project has been developed by 
+Dr John Towse (Department of Psychology, Lancaster University) [Maybe best not to have URL’s to web pages bacause these might change]
+Dr Rob Davies (Department of Psychology, Lancaster University
+Ben Gooding (School of Computing and Communications, Lancaster University)
+
+Code development work for the project is hosted at: https://github.com/Ben2917/LUSTRE
+
+Supported by a Teaching Development Grant from the Faculty of Science and Technology, Lancaster University. A previous version of this project was support by a CETL mini award from the Department of Maths and Statistics, Lancaster University
+</p>'; // Reading this from a file would be better
+        $page->save();
+
+        $page = new SimplePagesPage;
+        $page->modified_by_user_id = current_user()->id;
+        $page->created_by_user_id = current_user()->id;
+        $page->is_published = 1;
+        $page->parent_id = 0;
+        $page->title = 'More Info and Contact';
+        $page->slug = 'contact';
+        $page->text = '<p>Welcome to LUSTRE. LUSTRE stands for Lancaster University STatistics REsources, and is an online catalog and repository designed to encourage best practice in data management and open data for students in Psychology and related disciplines. LUSTRE holds project information and digital resources.
+
+LUSTRE has been designed to meet several teaching and research aims:
+
+a) LUSTRE provides a platform to train students in data management and encourage open data standards. We believe that in order to foster open data practices and illustrate the benefits of good data stewardship, it is vital that students have experience and training in data cataloguing. LUSTRE helps bring data management into a teaching curriculum and highlight the benefits that this can bring.
+
+b) LUSTRE increases the availability and functionality of student projects by providing an online catalog. We hope to improve quantitative analytic skills among students, through a forum that organises project data. By curating projects, it becomes easier to envisage how data that fuel them cannot be recycled and used in teaching. That is, to the extent that the teaching of analytic skills and approaches can be embedded in real world data, peer-created and managed, we hope to increase engagement in data science skills.
+
+c) LUSTRE offers a project repository that acts as an informational resource that can be interrogated and used to by students to learn about past projects pics and details -  what projects have been attempted, who supervised them, what was undertaken, and how. All of this information can help students make better informed choice about their own project scope.
+
+LUSTRE builds on the omeka platform providing a free, flexible, and open source web-publishing platform for the display of digital collections and exhibitions</p>';
         $page->save();
 
         $this->_installOptions();
@@ -93,80 +125,6 @@ class SimplePagesPlugin extends Omeka_Plugin_AbstractPlugin
         $db->query($sql);
 
         $this->_uninstallOptions();
-    }
-
-    /**
-     * Upgrade the plugin.
-     *
-     * @param array $args contains: 'old_version' and 'new_version'
-     */
-    public function hookUpgrade($args)
-    {
-        $oldVersion = $args['old_version'];
-        $newVersion = $args['new_version'];
-        $db = $this->_db;
-
-        // MySQL 5.7+ fix; must do first or else MySQL complains about any other ALTER
-        if ($oldVersion < '3.0.7') {
-            $db->query("ALTER TABLE `$db->SimplePagesPage` ALTER `inserted` SET DEFAULT '2000-01-01 00:00:00'");
-        }
-
-        if ($oldVersion < '1.0') {
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `is_published` )";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `inserted` ) ";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `updated` ) ";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `add_to_public_nav` ) ";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `created_by_user_id` ) ";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `modified_by_user_id` ) ";
-            $db->query($sql);    
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD `order` INT UNSIGNED NOT NULL ";
-            $db->query($sql);
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `order` ) ";
-            $db->query($sql);
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD `parent_id` INT UNSIGNED NOT NULL ";
-            $db->query($sql);
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD INDEX ( `parent_id` ) ";
-            $db->query($sql);
-            
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD `template` TINYTEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ";
-            $db->query($sql);
-        }
-
-        if ($oldVersion < '1.3') {
-            $sql = "ALTER TABLE `$db->SimplePagesPage` ADD `use_tiny_mce` TINYINT(1) NOT NULL";
-            $db->query($sql);
-        }
-
-        if ($oldVersion < '2.0') {
-            $db->query("ALTER TABLE `$db->SimplePagesPage` DROP `add_to_public_nav`");
-            delete_option('simple_pages_home_page_id');
-        }
-
-        if ($oldVersion < '3.0.2') {
-            $db->query("ALTER TABLE `$db->SimplePagesPage` MODIFY `text` MEDIUMTEXT COLLATE utf8_unicode_ci");
-        }
-    }
-
-    /**
-     * Add the translations.
-     */
-    public function hookInitialize()
-    {
-        add_translation_source(dirname(__FILE__) . '/languages');
     }
 
     /**
@@ -273,6 +231,9 @@ class SimplePagesPlugin extends Omeka_Plugin_AbstractPlugin
      */
     public function filterAdminNavigationMain($nav)
     {
+
+        console_log(url('simple-pages'));
+
         $nav[] = array(
             'label' => __('Simple Pages'),
             'uri' => url('simple-pages'),
